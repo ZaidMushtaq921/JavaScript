672506fa6c4364fe4814717f6622a12cf1e30a2d
Zaid mushtaq <mushtaqzaid589@ptuniv.edu.in>
2024-03-16 13:03:39 +0530

    Conversion done

---
+++ 03conversionOperations.js
@@ -0,0 +1,53 @@
+
+
+//=======================================================================
+//let score = "33"
+// let score = "33abc"
+// let score = "zaid"
+// let score = true
+// let score = null
+// let score = NaN
+// let score = undefined
+
+/*
+console.log(typeof(score))
+ let conversedScore = Number(score)
+ console.log("The converted value is "+ conversedScore)
+
+*/
+//"33" => output was  number
+// "33abc" => output was NaN
+// "zaid" => output was NaN
+// true => output was boolean
+// null => output was 0
+// NaN => output was NaN
+// undefine => output was NaN
+
+
+//=======================================================================
+
+
+// similerly we can convert into boolean also 
+
+//let score = "33";
+// let score = "";
+// let score = 1;
+
+// let boolScore= Boolean(score);
+// console.log("The converted value is "+ boolScore)
+
+ //"33"=> output was true
+ // ""=> output was false
+ // 1=> output was true
+
+ //=======================================================================
+
+//  let score = "33";
+
+//  let boolScore= String(score);
+
+//  console.log("The converted value is "+ boolScore)
+//  console.log("The converted value is "+ typeof (boolScore));
+
+//   The converted value is  33
+//   The converted value is  string
