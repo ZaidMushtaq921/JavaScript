3a7691fdc1bad4695ec1836b4020361abb2e8311
Zaid mushtaq <mushtaqzaid589@ptuniv.edu.in>
2024-03-16 14:43:10 +0530

    comparison done

---
+++ comparison.js
@@ -0,0 +1,29 @@
+// ************** comparison operators *****************
+// ==  equal to         - returns true if the operands are equal
+// != or <=> not equal   - returns true if the operands are not equal
+// >  greater than      - returns true if the left operand is greater than right operand
+// >= or >==            - returns true if  the left operand is greater than or equal to the right operand
+// < less then           - returns true if the left operand is less than the right operand
+// <= or <<=             - returns true if the left operand is less than or equal to the right operand.
+
+
+
+ //examples are
+ /*
+console.log(2 == 2);    //true
+
+console.log("Hello" === "Hello" );     //true because it checks both value and type of data, so here '===' will return true as both are same
+
+console.log(3 > 3);      //false
+
+console.log("hello" < "world");     // false because  string comparisons use lexicographic order, so it compares character by character till it finds a mismatch
+
+console.log("Hello" == "hello");     // false because of case sensitivity
+
+
+console.log(4 >= 5);       // false
+console.log(6 <= 6);        // true
+
+console.log(null == 0); // false
+console.log(undefined == 0); // false
+*/
