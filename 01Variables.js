04d801ce9a96688ead55af8883c51d6177765d45
Zaid mushtaq <mushtaqzaid589@ptuniv.edu.in>
2024-03-16 11:54:44 +0530

    variables

---
+++ 01Variables.js
@@ -0,0 +1,7 @@
+const userId = 123;
+let name = "John Doe";
+var age = 25; // var is a function scoped variable, let and const are block
+gender = "male"; // not recommended as it will be global if not in strict mode
+
+console.log(userId);
+console.table([name, age, gender]);
